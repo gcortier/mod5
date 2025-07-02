c9f85eb00d244ff611f301800e7b91a4bfbefe4f
gcortier <contact@gcortier.com>
2025-07-02 18:15:14 +0200

    maj

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from loguru import logger
 import requests
@@ -5,10 +6,12 @@ import requests
 st.title("Calculateur de carré")
 
 number = st.number_input("Entrez un entier", step=1, format="%d")
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 if st.button("Calculer le carré"):
     try:
-        response = requests.post("http://backend:8000/calcul", json={"number": int(number)})
+        st.write(f"Envoi de la requête à l'API : {API_URL}/calcul avec le nombre {int(number)}")
+        response = requests.post(f"{API_URL}/calcul", json={"number": int(number)})
         if response.status_code == 200:
             result = response.json()["result"]
             st.success(f"Le carré de {int(number)} est {result}")
